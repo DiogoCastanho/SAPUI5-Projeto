ae764cb49ddd02436dd60fe3585b7c626d91131e
Diogo Castanho <diogodev06@gmail.com>
2026-01-29 09:31:14 +0000

    Added some comments

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -7,12 +7,10 @@ sap.ui.define([
   return BaseController.extend("bs.ui5.projeto.controller.Detail", {
 
     onInit: function () {
-      // Liga o handler à rota "detail" (routing definido no manifest.json)
       this.getRouter().getRoute("detail").attachPatternMatched(this._onMatched, this);
     },
 
     _onMatched: function (oEvent) {
-      // Lê os argumentos da rota e faz element binding à entidade específica
       var oArgs = oEvent.getParameter("arguments");
       var sPath = "/Disciplinas/" + oArgs.discIndex + "/recursos/" + oArgs.resIndex;
       this.getView().bindElement({ path: sPath });
@@ -25,7 +23,6 @@ sap.ui.define([
       if (sPreviousHash !== undefined) {
         window.history.go(-1);
       } else {
-        // Volta para "home" e substitui a hash (para não duplicar histórico)
         this.getRouter().navTo("home", {}, true);
       }
     }
